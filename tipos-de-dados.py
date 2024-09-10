354e4655e859708557ef425d483ecd15e06c4e00
safira rodrigues <safirarodrigues.souza08@gmail.com>
2024-09-10 10:53:02 -0300

    Add files via upload

---
+++ tipos-de-dados.py
@@ -0,0 +1,13 @@
+# tipos de dados em pyton
+# inteiros
+inteiro = 10
+print(inteiro)
+# floats
+float = 10.5
+print(float)
+# strings
+string = "olá, mundo!"
+print(string)
+# booleanos
+booleano = True
+print(booleano)
